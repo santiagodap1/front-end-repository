48f4a52d0f88e745c6fcb6d3bafd4b789b74e802
santiagodap1 <santiagodeabreu430@gmail.com>
2024-02-08 20:17:51 +0000

    aula2

---
+++ aula-2/index.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            productName: 'Reserve um cruzeiro para a lua',
+            productDescription: 'Navegue até à lua na nossa luxuosa nave espacial e assista aos astronautas trabalhando no espaço.',
+    // additional properties later
+    }
+    },
+});
